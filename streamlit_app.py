f332b68f02fae88660afe5cce50c65f1f732df74
Louis M <91482309+mataluis2k@users.noreply.github.com>
2023-06-26 07:48:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 
-def generate_response(uploaded_file, openai_api_key, query_text):
+def generate_response(uploaded_file, openai_api_key, query_text, context_text):
     # Load document if file is uploaded
     if uploaded_file is not None:
         documents = [uploaded_file.read().decode()]
@@ -20,15 +20,18 @@ def generate_response(uploaded_file, openai_api_key, query_text):
         retriever = db.as_retriever()
         # Create QA chain
         qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-        return qa.run(query_text)
+        question = "'" + context_text + "'" + query_text
+        return qa.run(question)
 
 
 # Page title
-st.set_page_config(page_title='🦜🔗 Ask the Doc App')
-st.title('🦜🔗 Ask the Doc App')
+st.set_page_config(page_title='🦜🔗 Ask the Doc Enhancer')
+st.title('🦜🔗 Ask the Doc Enhancer')
 
 # File upload
 uploaded_file = st.file_uploader('Upload an article', type='txt')
+# Context text
+context_text = st.text_input('Provide Context:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
 # Query text
 query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
 
@@ -39,7 +42,7 @@ with st.form('myform', clear_on_submit=True):
     submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
     if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Calculating...'):
-            response = generate_response(uploaded_file, openai_api_key, query_text)
+            response = generate_response(uploaded_file, openai_api_key, query_text, context_text)
             result.append(response)
             del openai_api_key
 
